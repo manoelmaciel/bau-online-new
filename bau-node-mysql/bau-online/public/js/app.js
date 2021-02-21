0f9b044b3da96a24f5aae6d4bb5c126e9286f554
Manoel Braz Maciel <manoelbraz.maciel@gmail.com>
2021-02-21 20:24:26 -0300

    Inserindo ... O frontend do projeto Contacts!

---
+++ bau-node-mysql/bau-online/public/js/app.js
@@ -0,0 +1,10 @@
+// ./public/js/app.js
+
+// Log
+// console.log ( "1.1 - Cheguei aqui ... ./public/js/app.js!" )
+
+angular.module("contact", ['ngRoute']);
+
+// Log
+// console.log("1.2 - Cheguei aqui ... ./public/js/app.js!");
+
