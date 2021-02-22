541291c0b8faf4b54d6f759a39f85cc9e7012d7b
Manoel Braz Maciel <manoelbraz.maciel@gmail.com>
2021-02-21 21:44:33 -0300

    Backend ... GET /musicas e GET /notas funcionando!

--- bau-node-mysql/bau-online/routes/index.js
+++ bau-node-mysql/bau-online/routes/index.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET /notas */
 router.get('/musicas', function(req, res, next) {
-  console.log('Banco ...')
+  // console.log('Banco ...')
   req.getConnection((err, connection) => {
     connection.query('SELECT * FROM tb_musicas', [], (err, result) => {
+      if (err) {
+        return res.status(400).json(err)
+      } 
+      return res.status(200).json(result)
+    })
+  });
+  // res.render('index', { title: 'Express' });
+});
+
+/* GET /notas */
+router.get('/notas', function(req, res, next) {
+  // console.log('Banco ...')
+  req.getConnection((err, connection) => {
+    connection.query('SELECT * FROM tb_notas', [], (err, result) => {
       if (err) {
         return res.status(400).json(err)
       } 
